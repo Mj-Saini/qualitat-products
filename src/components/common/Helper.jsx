bd6263f3a7cf3b96868e1bd96d1c462a55cb78b9
mj-saini <manojsaini97285@gmail.com>
2025-09-04 09:14:32 +0530

    update

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -72,6 +72,7 @@ import testimonial2 from '../../assets/images/png/parminder.png'
 import testimonial3 from '../../assets/images/png/varun-komboj.png'
 import testimonial4 from '../../assets/images/png/rajvir-singh.png'
 import testimonial5 from '../../assets/images/png/vikas-testimonial.png'
+import testimonial6 from '../../assets/images/png/NoorPreetinder.png'
 
 
 // PRODUCTS IMAGES
@@ -324,10 +325,16 @@ export const demandsList = [
         name: "Rajbir Singh Aulakh",
         comment: "	I am Rajbir Singh Aulakh, owner of Aulakh Dairy Farm, located in VPO Nissing, District Karnal, Haryana. I have been engaged in dairy farming since 2003, currently managing a herd of 100 animals. When I started, the average milk yield per animal was around 15 liters, and today in 2025, with two-time milking, we have proudly reached up to 60 liters per animal. This growth and success have been possible thanks to the constant support and guidance of Dr. Anil Berry Sir, whose expertise has played a key role in transforming my farm. Qualitat Products has always kept us updated on time and provided valuable guidance whenever needed. I sincerely thank Qualitat Products for being a reliable partner in our dairy journey."
     },
+  
     {
         img: testimonial5,
         name: "Vikas Bishnoi",
         comment: "My name is Vikas Bishnoi from vpo Sardarpura, tehsil Abohar, dist- Fazilka, Punjab, and my farm is known as Sardarpura Dairy Farm. Initially, I had a very small farm and it was extremely difficult for me to manage it, as I had no proper guidance about dairy farming. In 2021, I came to know about World Wide Sires through Dr. Anil Berry, the owner of Qualitat Products. Dr. Berry guided me thoroughly about WWS and provided me with the best-quality semen.Thanks to his support, today my farm has grown to a top-level dairy farm with more than 100 HF cows. I sincerely thank Dr. Anil Berry and Qualitat Products for always providing me with the best semen, excellent guidance, and consistent top-class service."
+    },
+      {
+        img: testimonial6,
+        name: "Dr. Noorpreetinder Singh (Dr. Noor Brar)",
+        comment: "I am Dr. Noorpreetinder Singh (Dr. Noor Brar), proud owner of Veerjindera Dairy Farm, a three-generation legacy farm established in 1971 by my grandfather, S. Tej Singh (Sarpanch). Today, our farm houses 150 cattle and stands as a symbol of dedication and progress. We have been using imported semen since 1988, with our first imported semen calf born in 1989. Continuing this journey of improvement, we started using World Wide Sires semen in 2013, which has significantly contributed to the genetic strength and productivity of our herd.Located in Village Channuwal, Tehsil Baghapurana, District Moga, our farm is recognized among the top-level farms in Punjab. This success would not have been possible without the constant support of Dr. Anil Berry and the excellent service of Qualitat Products. Dr. Berry has always provided me with the best guidance, regular farm visits, and established a strong, trustworthy relationship. I sincerely thank him and his team for helping us achieve excellence in dairy farming."
     },
    
 
